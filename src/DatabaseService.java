2990b8e9feb3d6a5d77f9087f26ffdd435ac7cd5
AMAN MUNJEWAR <munjewaraman07@gmail.com>
2023-05-18 16:34:46 +0530

    Created a new class to pack all the database functionalities.

---
+++ src/DatabaseService.java
@@ -0,0 +1,28 @@
+import java.sql.*;
+
+public class DatabaseService {
+    String url = "jdbc:mysql://localhost:3306/banking";
+    String user = "root";
+    String pass = "root";
+
+    public void getInfo() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            Connection connection = DriverManager.getConnection(url,user,pass);
+            Statement statement = connection.createStatement();
+
+            ResultSet resultSet = statement.executeQuery("select * from user");
+
+            while (resultSet.next()){
+                System.out.println(resultSet.getInt(1)+" "+resultSet.getString(2));
+            }
+
+            connection.close();
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,7 @@
-import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
-        String url = "jdbc:mysql://localhost:3306/banking";
-        String user = "root";
-        String pass = "root";
-
-        try{
-            Class.forName("com.mysql.cj.jdbc.Driver");
-
-            Connection connection = DriverManager.getConnection(url,user,pass);
-            Statement statement = connection.createStatement();
-
-            ResultSet resultSet = statement.executeQuery("select * from user");
-
-            while (resultSet.next()){
-                System.out.println(resultSet.getInt(1)+" "+resultSet.getString(2));
-            }
-
-            connection.close();
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
+        DatabaseService service = new DatabaseService();
+        service.getInfo();
     }
 }
