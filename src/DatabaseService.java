f7c9620879e17088a8b50699e7c618331df03166
AMAN MUNJEWAR <munjewaraman07@gmail.com>
2023-05-18 16:45:27 +0530

    DatabaseService optimization session 0.0

--- src/DatabaseService.java
+++ src/DatabaseService.java
@@ -4,26 +4,33 @@ public class DatabaseService {
     String url = "jdbc:mysql://localhost:3306/banking";
     String user = "root";
     String pass = "root";
+    Connection connection;
+    Statement statement;
+    ResultSet resultSet;
 
-
-    public void getInfo() {
+    DatabaseService(){
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
 
-            Connection connection = DriverManager.getConnection(url,user,pass);
-            Statement statement = connection.createStatement();
-
-            ResultSet resultSet = statement.executeQuery("select * from user");
-
-            while (resultSet.next()){
-                System.out.println(resultSet.getInt(1)+" "+resultSet.getString(2));
-            }
-
-            connection.close();
-
+            connection = DriverManager.getConnection(url,user,pass);
+            statement = connection.createStatement();
         }
         catch (Exception e){
             e.printStackTrace();
         }
+    }
+
+    public void getInfo() {
+        try{
+            resultSet = statement.executeQuery("select * from user");
+
+            while (resultSet.next()){
+                System.out.println(resultSet.getInt(1)+" "+resultSet.getString(2));
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,5 @@ public class Main {
     public static void main(String[] args) {
         DatabaseService service = new DatabaseService();
         service.getInfo();
-
-
     }
 }
